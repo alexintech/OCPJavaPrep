9c3615abd8d2353147059fbcda83b7c5616ed386
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2016-07-03 12:36:38 +0600

    Using this for accessing variables of the outer classes.

--- AdvancedClassDesign/InnerClass.java
+++ AdvancedClassDesign/InnerClass.java
@@ -13,6 +13,20 @@ class Outer {
     Inner inner = new Inner();
     inner.go();
   }
+
+  private int x = 10;
+  class B {
+    private int x = 20;
+    class C {
+      private int x = 30;
+      public void allTheX() {
+        System.out.println(x);               // 30
+        System.out.println(this.x);          // 30
+        System.out.println(B.this.x);        // 20
+        System.out.println(Outer.this.x);    // 10
+      }
+    }
+  }
 }
 
 public class InnerClass {
@@ -24,5 +38,10 @@ public class InnerClass {
     Outer.Inner inner = outer.new Inner(); // you need to create Outer object
                                            // to create Inner object
     inner.go();   // prints "Hi" three times
+
+    Outer a = new Outer();
+    Outer.B b = a.new B();
+    Outer.B.C c = b.new C();
+    c.allTheX();
   }
 }
