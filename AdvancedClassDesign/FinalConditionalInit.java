32841ce44f7b7f3430fdd4fbffbe268ee5ef16f0
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2016-07-19 17:04:53 +0600

    Initialization final variables within conditional blocks.

---
+++ AdvancedClassDesign/FinalConditionalInit.java
@@ -0,0 +1,34 @@
+public class FinalConditionalInit {
+  final int finalVar1;
+  final int finalVar2;
+  final int finalVar3;
+  FinalConditionalInit(double a, double b) {
+    /*
+    if (a > b)
+      finalVar1 = 20;
+    else if (b >= a)
+      finalVar1 = 30;   // error: variable finalVar1 might not have been initialized
+    */
+    if (a > b)
+      finalVar1 = 20;
+    else
+      finalVar1 = 30;   // OK
+
+    if (1 > 2)
+      finalVar2 = 10;
+    else if (100 > 10)
+      finalVar2 = 20;   // OK
+
+    finalVar3 = 100;
+    /*
+    if (a > b)
+      finalVar1 = 20;   // error: variable finalVar1 might already have been assigned
+    else if (b >= a)
+      finalVar1 = 30;   // error: variable finalVar1 might already have been assigned
+    */
+  }
+  public static void main(String[] args) {
+
+  }
+}
+
