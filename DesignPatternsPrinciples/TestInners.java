a590715bf3fc990567f56bf374148d1635634b8e
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2016-08-13 17:50:55 +0600

    Inner class shadows top-level class

---
+++ DesignPatternsPrinciples/TestInners.java
@@ -0,0 +1,25 @@
+class A {
+  void m() {
+    System.out.println("outer");
+  }
+}
+
+public class TestInners {
+  public static void main(String[] args) {
+    new TestInners().go();
+  }
+  void go() {
+    new A().m();      // prints middle
+    class A {
+      void m() {
+        System.out.println("inner");
+      }
+    }
+  }
+  class A {
+    void m() {
+      System.out.println("middle");
+    }
+  }
+}
+
