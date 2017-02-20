3df688603fff993cc8f00018aea68d621b5f35ab
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2017-02-20 07:50:26 +0600

    Using HashSet and TreeSet

---
+++ GenericsCollections/SetWorking.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class SetWorking {
+  public static void usingHashSet() {
+    Set<Integer> set = new HashSet<>();
+    boolean b1 = set.add(66);   // true
+    boolean b2 = set.add(10);   // true
+    boolean b3 = set.add(66);   // false
+    boolean b4 = set.add(8);    // true
+    for (Integer integer : set)
+      System.out.print(integer + ",");  // 66,8,10,
+    System.out.println();
+
+    Iterator<Integer> iter = set.iterator();
+    while (iter.hasNext()) {
+      Integer integer = iter.next();
+      System.out.print(integer + "|");
+    }
+    System.out.println();
+  }
+
+  public static void usingTreeSet() {
+    Set<Integer> set = new TreeSet<>();
+    boolean b1 = set.add(66);   // true
+    boolean b2 = set.add(10);   // true
+    boolean b3 = set.add(66);   // false
+    boolean b4 = set.add(8);    // true
+    for (Integer integer : set)
+      System.out.print(integer + ",");  // 8,10,66,
+    System.out.println();
+
+    // before Java 5
+    Iterator iter = set.iterator();
+    while (iter.hasNext()) {
+      Integer integer = (Integer) iter.next();
+      System.out.print(integer + "|");
+    }
+    System.out.println();
+  }
+
+
+  public static void main(String[] args) {
+    usingHashSet();
+    usingTreeSet();
+  }
+}
+
