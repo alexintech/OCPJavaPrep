f2cda55a46e8a8763264e638641f37cf91beff6c
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2017-02-20 08:01:54 +0600

    Using NavigableSet interface

--- GenericsCollections/SetWorking.java
+++ GenericsCollections/SetWorking.java
@@ -38,10 +38,19 @@ public class SetWorking {
     System.out.println();
   }
 
+  public static void usingNavigableSet() {
+    NavigableSet<Integer> set = new TreeSet<>();
+    for (int i = 1; i <= 20; i++) set.add(i);
+    System.out.println(set.lower(10));    // 9
+    System.out.println(set.floor(10));    // 10
+    System.out.println(set.ceiling(20));  // 20
+    System.out.println(set.higher(20));   // null
+  }
 
   public static void main(String[] args) {
     usingHashSet();
     usingTreeSet();
+    usingNavigableSet();
   }
 }
 
