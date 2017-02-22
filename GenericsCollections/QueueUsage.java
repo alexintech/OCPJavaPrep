ee18b6d272f6c75ae534301d914ce5ad856dc35c
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2017-02-22 07:45:04 +0600

    Queue usage

---
+++ GenericsCollections/QueueUsage.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class QueueUsage {
+  public static void main(String[] args) {
+    Queue<Integer> queue = new ArrayDeque<>();
+    System.out.println(queue.offer(10));  // true
+    System.out.println(queue.offer(4));   // true
+    System.out.println(queue.peek());     // 10
+    System.out.println(queue.poll());     // 10
+    System.out.println(queue.poll());     // 4
+    System.out.println(queue.peek());     // null
+
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+    stack.push(10);
+    stack.push(4);
+    System.out.println(stack.peek());     // 4
+    System.out.println(stack.poll());     // 4
+    System.out.println(stack.poll());     // 10
+    System.out.println(stack.peek());     // null
+  }
+}
+
