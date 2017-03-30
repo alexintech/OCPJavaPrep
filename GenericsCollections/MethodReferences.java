476d8e403d28b789719bf9554fac5296951fdfd0
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2017-03-30 07:51:30 +0600

    Using method references in Java 8

---
+++ GenericsCollections/MethodReferences.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.function.*;
+
+public class MethodReferences {
+  private static class Duck {
+    private int weight;
+    private String name;
+  }
+
+  static class DuckHelper {
+    public static int compareByWeight(Duck d1, Duck d2) {
+      return d1.weight - d2.weight;
+    }
+    public static int compareByName(Duck d1, Duck d2) {
+      return d1.name.compareTo(d2.name);
+    }
+  }
+
+  public static void main(String[] args) {
+    Comparator<Duck> byWeight = (d1, d2) -> DuckHelper.compareByWeight(d1, d2);
+    Comparator<Duck> byWeight2 = DuckHelper::compareByWeight;
+
+    Consumer<List<Integer>> methodRef1 = Collections::sort;
+    Consumer<List<Integer>> lambda1 = l -> Collections.sort(l);
+    List<Integer> sortedList = Arrays.asList(5, 3, 9, 2);
+    methodRef1.accept(sortedList);
+    System.out.println(sortedList);
+
+    String str = "abc";
+    Predicate<String> methodRef2 = str::startsWith;
+    Predicate<String> lambda2 = s -> str.startsWith(s);
+    System.out.println(methodRef2.test("ab"));
+
+    Predicate<String> methodRef3 = String::isEmpty;
+    Predicate<String> lambda3 = s -> s.isEmpty();
+    System.out.println(methodRef3.test("abc"));
+
+    Supplier<ArrayList> methodRef4 = ArrayList::new;
+    Supplier<ArrayList> lambda4 = () -> new ArrayList();
+    System.out.println(methodRef4.get());
+  }
+}
+
