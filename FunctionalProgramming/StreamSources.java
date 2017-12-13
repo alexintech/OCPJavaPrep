41d6ef23a914026e43a4b9fce33e2ee5edae72c5
Alexander Cherepanov <xandr.cherepanov@gmail.com>
2017-12-13 07:12:08 +0600

    Stream source methods

---
+++ FunctionalProgramming/StreamSources.java
@@ -0,0 +1,19 @@
+import java.util.stream.*;
+import java.util.*;
+
+public class StreamSources {
+  public static void main(String[] args) {
+    Stream<String> empty = Stream.empty();          // count = 0
+    Stream<Integer> singleElement = Stream.of(1);   // count = 1
+    Stream<Integer> fromArray = Stream.of(1, 2, 3); // count = 3
+
+    List<String> list = Arrays.asList("a", "b", "c");
+    Stream<String> fromList = list.stream();
+    Stream<String> fromListParallel = list.parallelStream();
+
+    // infinite streams
+    Stream<Double> randoms = Stream.generate(Math::random);
+    Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);
+  }
+}
+
